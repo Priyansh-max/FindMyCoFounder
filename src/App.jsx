6824e2a0448f24d7db1e6a8c55c15c89b8286dff
Priyansh-max <priyanshagarwal3381@gmail.com>
2025-02-05 19:41:39 +0530

    improvements

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Toaster } from 'sonner';
+
+function App() {
+  return (
+    <>
+      <Toaster position="top-right" richColors />
+      {/* rest of your app */}
+    </>
+  );
+}
+
+export default App; 
