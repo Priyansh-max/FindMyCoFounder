fb1cee96081de0b899cb27b9c892df787acb4c8a
Priyansh-max <priyanshagarwal3381@gmail.com>
2025-03-01 18:20:00 +0530

    route added

--- backend/routes/ideaRoutes.js
+++ backend/routes/ideaRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const { authenticateUser } = require('../middleware/auth');
-const { createIdea, updateIdea, getIdeabyUser, getIdeas, updateIdeaStatus, deleteIdea } = require('../controllers/ideaController');
+const { createIdea, updateIdea, getIdeabyUser, getIdeas, updateIdeaStatus, deleteIdea, getideabyID } = require('../controllers/ideaController');
 
 router.post('/create', authenticateUser, createIdea);
 router.put('/update', authenticateUser, updateIdea);
 router.get('/user', authenticateUser, getIdeabyUser);
 router.get('/', authenticateUser, getIdeas);
+router.get('/:id', authenticateUser, getideabyID);
 router.put('/status/:id', authenticateUser, updateIdeaStatus);
 router.delete('/:id', authenticateUser, deleteIdea);
 
