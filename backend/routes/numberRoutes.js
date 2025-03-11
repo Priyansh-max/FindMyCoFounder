c3b2424ce0669e9c5e05f8391d5088c5472e8acc
Priyansh-max <priyanshagarwal3381@gmail.com>
2025-03-11 17:11:46 +0530

    route added to get the number of application received in a particular idea

--- backend/routes/numberRoutes.js
+++ backend/routes/numberRoutes.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { authenticateUser } = require('../middleware/auth');
 const { getStats } = require('../controllers/numberController');
+const { getApplicationStats } = require('../controllers/numberController');
 
 // Route to get all statistics
 router.get('/stats', authenticateUser, getStats);
+router.get('/application-stats/:ideaId', authenticateUser, getApplicationStats);
 
 module.exports = router;
+
